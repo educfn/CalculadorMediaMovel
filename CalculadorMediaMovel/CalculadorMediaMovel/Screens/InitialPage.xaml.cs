62234d102c8b112198924803d34e81777a2c433a
Eduardo CFN <eduardo@verion.ind.br>
2021-06-22 11:16:33 -0300

    *Created .gitignore
    *ColetarDados() - Sucessfully populated the vector 'vetor'.

--- CalculadorMediaMovel/CalculadorMediaMovel/Screens/InitialPage.xaml.cs
+++ CalculadorMediaMovel/CalculadorMediaMovel/Screens/InitialPage.xaml.cs
@@ -12,10 +12,46 @@ namespace CalculadorMediaMovel.Screens
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class InitialPage : ContentPage
     {
+        private int index = 0;
+        private int[] vetor;
+
+
         public InitialPage()
         {
             InitializeComponent();
+
+            coletarDados();
         }
+
+        public void coletarDados() 
+        {
+            int tamanhoDoVetor = 0;
+            int valorInicial = 0;
+
+
+            //TO DO: Coletar o tamanho do vetor e valor inicial do usuario.
+            tamanhoDoVetor = 7;
+            valorInicial = 8;
+
+            //Inicializando o 'vetor'.
+            //TO DO: Implementar verificacao da variavel 'tamanhoDoVetor' para verificar se o valor esta zero ou negativo.
+            vetor = new int[tamanhoDoVetor];
+
+            //Populando array 'vetor'.
+            //TO DO: Verificar se 'valorInicial' esta com valor zero ou negativo.
+            for (int i = 0; i < vetor.Length;i++)
+            {
+                vetor[i] = valorInicial;
+            }
+        }
+
+        public void calcularMediaMovel()
+        {
+            coletarDados();
+
+
+        }
+
     }
 
 }
